31b89055ff94bb9552c2405b4713a5670420d8c6
R. Dyego <dyego@alu.ufc.br>
2015-07-27 23:34:27 -0300

    Added sails-mongo

--- config/connections.js
+++ config/connections.js
@@ -29,7 +29,7 @@ module.exports.connections = {
   *                                                                          *
   ***************************************************************************/
   localDiskDb: {
-    adapter: 'sails-disk'
+    adapter: 'sails-mongo'//'sails-disk'
   },
 
   /***************************************************************************
@@ -60,9 +60,10 @@ module.exports.connections = {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    user: '',
+    password: '',
+    database: 'activityoverlord',
+    schema: true
   },
 
   /***************************************************************************
