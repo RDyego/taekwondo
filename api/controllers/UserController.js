d8f8ed8d130b299e2ff0c61b9a5032a0a0c44d83
R. Dyego <dyego@alu.ufc.br>
2015-07-27 23:24:48 -0300

    Added api user

---
+++ api/controllers/UserController.js
@@ -0,0 +1,32 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'new': function (req, res, next) {
+		res.view();
+	},
+
+	index: function (req, res, next) {
+		User.find({}).exec(function (err, usersFound) {
+			if (err) return next(err);
+			res.view({ users: usersFound });
+		});
+	},
+	
+	create: function (req, res, next) {
+		var userViewModel = {
+			name: req.param('name'),
+			email: req.param('email')	
+		};
+		
+		User.create(userViewModel).exec(function (err, userCreated) {
+			if (err) res.redirect('/user/new');
+			res.redirect('/user/index');
+		});
+	}
+};
+
