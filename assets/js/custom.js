7f1a76978e879c02c729234b891f4d5e298db7b1
R. Dyego <dyego@alu.ufc.br>
2015-08-02 11:47:39 -0300

    Fixed layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,24 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('select').material_select();
 	$(".dropdown-button").dropdown({ hover: false });
+	$('.datepicker').pickadate({
+		// Strings and translations
+		monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+			'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+		monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+			'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+		weekdaysFull: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+		weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+		// Buttons
+		today: 'Hoje',
+		clear: 'Limpar',
+		close: 'Fechar',
+		// Formats
+		format: 'dd/mm/yyyy',
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 70 // Creates a dropdown of 15 years to control year
+	});
 	$('.modal-trigger').leanModal({
 		dismissible: true, // Modal can be dismissed by clicking outside of the modal
 		in_duration: 300, // Transition in duration
