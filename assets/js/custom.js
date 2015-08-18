357250b1d9d36e5c7b0c25be4e87cf28c1622e61
RDyego <dyego@alu.ufc.br>
2015-08-17 22:01:46 -0300

    Fixed layout mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
 			}
 		}
 	});
-	
+	$(".button-collapse").sideNav();
     $('select').material_select();
 	$(".dropdown-button").dropdown({ hover: false });
 	$('.datepicker').pickadate({
