8a2305271373261cdff81806eff1131a6100b5f7
RDyego <dyego@alu.ufc.br>
2015-09-30 20:54:38 -0300

    Removed blobAdapter

--- api/services/BlobAdapterService.js
+++ api/services/BlobAdapterService.js
@@ -1,25 +1,19 @@
 module.exports = {
 	blobAdapter: function () {
-		var db;
-		var uriMongo;
-
-		if (sails.config.environment === 'production') {
-			db = sails.config.connections.production;
-			uriMongo = db.url;
-		} else {
-			db = sails.config.connections.someMongodbServer;
-			uriMongo = 'mongodb://';
-			uriMongo += db.username ? db.username + ':' : '';
-			uriMongo += db.password ? db.password + '@' : '';
-			uriMongo += db.host + ':' + db.port;
-			uriMongo += '/' + db.database;
-		}
+		//var db = sails.config.connections.someMongodbServer;
+		var db = sails.config.connections.production;
+		
+		var uriMongo = 'mongodb://';
+		uriMongo += db.username ? db.username + ':' : '';
+		uriMongo += db.password ? db.password + '@' : '';
+		uriMongo += db.host + ':' + db.port;
+		uriMongo += '/' + db.database;
 
 		var blobAdapter = require('skipper-gridfs')({
 			maxBytes: 500000, //500kb
 			uri: uriMongo + '.photo'
 		});
-
+		
 		return blobAdapter;
 	}
 }
